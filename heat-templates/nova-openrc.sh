b713740a90771e132edebbd01c6d11b269c1e3af
Diego Casati <diego.prottacasati@emc.com>
2016-12-12 21:10:46 -0500

    - add an OpenStack Heat template that creates a basic stack with FreeBSD Images. - add a template openrc.sh

---
+++ heat-templates/nova-openrc.sh
@@ -0,0 +1,8 @@
+unset ${!OS_*}
+export OS_AUTH_URL=https://192.168.219.120:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_USERNAME=admin
+export OS_PASSWORD=NotMyPassw0rd
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_DOMAIN_NAME=default
